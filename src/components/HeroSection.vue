b5c5a4b4695731a1d82c41f91090aee540364656
mrdickywahyu <mrdickywahyu@gmail.com>
2020-10-29 16:32:53 +0700

    first commit

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,38 @@
+<template>
+     <!-- Hero Section Begin -->
+    <section class="hero-section">
+        <carousel class="hero-items" :items="1" :nav="false">
+            <div class="single-hero-items set-bg" style="background-image: url('/img/hero-1.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <img src="img/logo_website_morayya1.png" alt="" />
+                           
+                            <p>
+                                Moms And Baby Wear
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </carousel>
+    </section>
+    <!-- Hero Section End -->
+</template>
+
+
+
+<script>
+
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+    name: 'HeroSection',
+    components : {
+        carousel
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+   <HeaderMorayya />
+   <HeroSection />
+   <ProductMorayya />
+   <InstaMorayya />
+   <FooterMorayya />
+
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMorayya from '@/components/HeaderMorayya.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import ProductMorayya from '@/components/ProductMorayya.vue'
+import InstaMorayya from '@/components/InstaMorayya.vue'
+import FooterMorayya from '@/components/FooterMorayya.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+   HeaderMorayya,
+   HeroSection,
+   ProductMorayya,
+   InstaMorayya,
+   FooterMorayya
+  }
+}
+</script>
